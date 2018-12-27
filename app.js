9ad7fcddaec4307d9818831bc0fdf0e9ade1fd89
Sai Prasad <sai.prasad@rocketmail.com>
2018-12-27 17:38:16 +0530

    adding app.js with skeleton structure

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book class: represent book with title, author, isbn
+
+// UI class: handle UI tasks
+
+// Store Class: handles local storage
+
+// Event: display books
+
+// Event: add book
+
+// Event: remove book
