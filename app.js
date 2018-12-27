538979c81438b7dc87ee629efc995c082f97c90f
Sai Prasad <sai.prasad@rocketmail.com>
2018-12-27 20:13:52 +0530

    Added functionality for add, remove and alerting

--- app.js
+++ app.js
@@ -41,6 +41,39 @@ class UI {
         `;
         list.appendChild(row);
     }
+
+    static deleteBook(el) {
+        const btn = el.classList.value
+        console.log(typeof(btn))
+        if(btn.includes("delete")) {
+            console.log(el.parentElement.parentElement)
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+
+        container.insertBefore(div, form);
+        setTimeout(() => 
+            document.querySelector('.alert').remove(), 900
+        )
+        
+    }
+
+    static clearFields() {
+        // get form values
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+
+    }
+
 }
 // Store Class: handles local storage
 
@@ -48,5 +81,36 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: add book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    // get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Book data missing','danger');
+    }
+    else {
+    // instantiate book
+        const book = new Book(title, author, isbn);
+        
+        // add to UI list
+        UI.addBookToList(book);
+
+        // clear form fields
+        UI.clearFields();
+        UI.showAlert('Book added','success');
+    }
+
+
+
+});
 
 // Event: remove book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+    UI.showAlert('Book removed','warning');
+})
+
